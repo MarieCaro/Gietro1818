7f0dc996ceecb6505ab033438690563cd2a12efd
Marie-Caroline <marie-caroline.schmied@chartes.psl.eu>
2019-04-12 15:58:00 +0200

    Ajout des jointures entre type et personne

--- app/models.py
+++ app/models.py
@@ -2,43 +2,34 @@ from app import db
 
 
 class Lieu(db.Model):
+    __tablename__ = "lieu"
     id = db.Column(db.Integer, primary_key=True)
     nom = db.Column(db.TEXT, index=True, unique=True)
     commune = db.Column(db.TEXT, index=True, unique=True)
-    personnes = db.relationship('Personne', backref='origine', lazy='dynamic')
-
-    def __repr__(self):
-        return '<Lieu {}>'.format(self.username)
 
 
 class Type(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    label = db.Column(db.TEXT, index=True, unique=True)
-    personne = db.relationship('Personne', backref='genre', lazy='dynamic')
+    __tablename__ = "type"
+    typeId = db.Column(db.Integer, primary_key=True)
+    type_label = db.Column(db.TEXT, index=True, unique=True)
 
-    def __repr__(self):
-        return '<Type {}>'.format(self.username)
 
 
 class Personne(db.Model):
+    __tablename__ = "personne"
     id = db.Column(db.Integer, primary_key=True)
     nom = db.Column(db.TEXT, index=True)
     prenom = db.Column(db.TEXT, index=True)
     age = db.Column(db.Integer, index=True)
-    de = db.Column (db.Integer, db.ForeignKey("lieu.id"))
-    domicile = db.Column (db.Integer, db.ForeignKey("lieu.id"))
+    domicile = db.Column(db.Integer, db.ForeignKey("lieu.id"))
+    de = db.Column(db.Integer, db.ForeignKey("lieu.id"))
     travail = db.Column(db.TEXT)
-    type = db.Column (db.Integer, db.ForeignKey("type.id"))
+    type_id = db.Column(db.Integer, db.ForeignKey("type.typeId"))
     fonction = db.Column(db.TEXT)
     donnees_biographiques = db.Column(db.TEXT)
     levee_de_corps = db.Column(db.TEXT)
     informations_complementaires = db.Column(db.TEXT)
     mission_debacle = db.Column(db.TEXT)
     correspondance = db.Column(db.TEXT)
-
-    def __repr__(self):
-        return '<Personne {}>'.format(self.username)
-
-
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
+    wikipedia = db.Column(db.TEXT)
+    type = db.relationship("Type")
