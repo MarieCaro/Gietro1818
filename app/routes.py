f4fcabad9da056f9a82249655e81d71bfb6ad943
Marie-Caroline <marie-caroline.schmied@chartes.psl.eu>
2019-04-12 15:58:36 +0200

    Ajout de la page personne et code pour retourner les infos de la base de données

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,7 @@
-from flask import render_template, url_for
-from werkzeug.urls import url_parse
+from flask import render_template, url_for, request
 from app import app
 from .models import Personne, Lieu, Type
-
+from sqlalchemy import or_
 
 @app.route('/')
 def accueil():
@@ -49,6 +48,36 @@ def recherche():
 def secours():
     return render_template('pages/secours.html')
 
+
 @app.route('/resultats')
-def resultats():
-    return render_template('pages/resultat.html')
+def search_results():
+    motclef = request.args.get("keyword", None)
+
+    results = []
+    titre = "Recherche"
+    if motclef:
+        results = Personne.query.filter(or_(
+            Personne.nom.like("%{}%".format(motclef)),
+            Personne.prenom.like("%{}%".format(motclef)),
+            Personne.age.like("%{}%".format(motclef)),
+            Personne.travail.like("%{}%".format(motclef)),
+            Personne.fonction.like("%{}%".format(motclef)),
+            Personne.donnees_biographiques.like("%{}%".format(motclef)),
+            Personne.informations_complementaires.like("%{}%".format(motclef)),
+            Personne.levee_de_corps.like("%{}%".format(motclef)),
+            Personne.mission_debacle.like("%{}%".format(motclef)),
+        )).order_by(Personne.nom.asc()).paginate()
+        titre = "Résultat pour la recherche '" + motclef + "'"
+
+    return render_template(
+        "pages/resultat.html",
+        results=results,
+        titre=titre,
+        keyword=motclef)
+
+@app.route("/notice/<int:personne_id>")
+def notice(personne_id):
+    noticep= Personne.query.get(personne_id)
+    return render_template("pages/personne.html", noticep=noticep)
+
+
