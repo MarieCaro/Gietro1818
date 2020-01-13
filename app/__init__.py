d06eb6c74a513be37de3103bdcbffe400bb39872
Marie-Caroline <marie-caroline.schmied@chartes.psl.eu>
2020-01-13 07:36:44 +0100

    Ajout du chemin de l'api

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify, Response, json, make_response
 from config import CONFIG
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
+from flask_restful import Api, Resource
 import os
 
 
@@ -12,6 +13,7 @@ static = os.path.join(chemin_actuel, "static")
 
 
 app = Flask(__name__, template_folder=templates, static_folder=static)
+api = Api(app=app)
 cors = CORS(app)
 app.config.from_object(CONFIG)
 db = SQLAlchemy(app)
@@ -22,4 +24,22 @@ app.config['SQLALCHEMY_ECHO'] = True
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
+class GetManifest(Resource):
+    def get(self, cote):
+        with open ("app/manifest/" + cote + ".json") as manifeste:
+            d = json.load(manifeste)
+            return make_response(jsonify(d), 200)
+
+class GetImage(Resource):
+    def get(self, cote):
+        return()
+
+
+api.add_resource(GetManifest, '/api/manifest/<cote>')
+
+
+
 from app import routes, models
+
+
+
